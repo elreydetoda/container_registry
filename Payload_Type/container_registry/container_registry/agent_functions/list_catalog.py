bab7f5afbf73a68215c88bec5fbcd51813b92d76
Alex Rodriguez <10230166+elreydetoda@users.noreply.github.com>
2026-02-27 16:26:12 -0500

    replicating variable

--- Payload_Type/container_registry/container_registry/agent_functions/list_catalog.py
+++ Payload_Type/container_registry/container_registry/agent_functions/list_catalog.py
@@ -123,7 +123,8 @@ class ListCatalog(CommandBase):
                     )
             elif resp.status_code == 400:
                 # for when a self-signed certificate is used to make https but not trusted by container
-                resp = requests.get(catalog_url.replace("http://","https://"), auth=auth, verify=verify_ssl, timeout=30)
+                catalog_url = catalog_url.replace("http://","https://")
+                resp = requests.get(catalog_url, auth=auth, verify=verify_ssl, timeout=30)
                 try:
                     # Parse JSON response
                     catalog_data = resp.json()
